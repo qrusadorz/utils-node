6587ed846e396bebb9dfd4246e99f969e6cdfa40
qrusadorz <qrusadorz@gmail.com>
2020-07-24 18:36:29 +0900

    chore

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import { arrayToObject } from "./src/array"
 import { objectToArray } from "./src/object"
-import { sleepForProtection } from "./src/sleep"
+import { sleep, sleepForProtection } from "./src/sleep"
 import { getPrivateMethodsInTest } from "./src/testHelper"
 
 // ESM syntax is supported.
 export {
 	arrayToObject,
 	objectToArray,
+	sleep,
 	sleepForProtection,
 	getPrivateMethodsInTest,
 }
